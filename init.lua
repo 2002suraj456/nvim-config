cc2f6ef6799614f53f63e4c595edce025bf2a6af
2002suraj456 <surajsoren456@gmail.com>
2026-07-30 00:51:26 +0530

    fuzzy finder.

--- init.lua
+++ init.lua
@@ -98,3 +98,18 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- plugins
+
+vim.pack.add({
+  'https://github.com/nvim-lua/plenary.nvim',
+  "https://github.com/nvim-telescope/telescope.nvim"
+})
+
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
